040b36561be7ddecbf69477d0e27845fe314160b
piyushMaker <supe.piyush40@gmail.com>
2023-11-24 20:28:51 +0530

    Create numpySlicing3D.py

---
+++ numpySlicing3D.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+arr3d = np.array([[[1,2,3], [1,2,3],[1,2,3]],[[4,5,6],[4,5,6],[4,5,6]],[[7,8,9],[7,8,9],[7,8,9]]])
+
+print(arr3d)
+print(" ")
+
+print(arr3d [:,0,0])  #output [1,4,7]
+
+#array slicing [Front2Back,ToptoBottom,RighttoLeft]
